7534e9c1c4b30bf6f4ca90d919f077268f349738
Eduard Donkeev <donkeev.ed@yandex.ru>
2021-02-16 11:16:38 +0300

    bug fixes 2

--- controllers/users.js
+++ controllers/users.js
@@ -6,22 +6,26 @@ const dataPath = path.join(__dirname, "..", "data", "users.json");
 const getUsers = (req, res) => {
   return getDataFromFile(dataPath)
     .then((users) => res.status(200).send(users))
-    .catch(() => res.status(500).send({ message: "Ошибка сервера" }));
+    .catch(() =>
+      res
+        .status(500)
+        .send({ message: "Не удалось получить информацию о пользователе" })
+    );
 };
 
 const getProfile = (req, res) => {
   return getDataFromFile(dataPath)
-    .then((users) => users.find((user) => user._id === req.params._id))
-    .then((user) => {
+    .then((users) => {
+      const user = users.find((user) => user._id === req.params.id);
       if (!user) {
-        return res.status(404).send({ message: "Нет пользователя с таким id" });
+        res.status(404).send({ message: `Нет пользователя с таким id` });
       }
 
       return res.status(200).send(user);
     })
     .catch(() =>
       res
-        .ststus(500)
+        .status(500)
         .send({ message: "Не удалось получить информацию о пользователе" })
     );
 };
